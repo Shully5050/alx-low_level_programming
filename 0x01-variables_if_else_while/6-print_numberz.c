1f89b60a720eef44b39ab83c52d984d38583b9e4
shully5050 <shullamiteibeakalam2019@gmail.com>
2022-09-10 21:09:08 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lower letter
+ * Return: Always 0 (Success)
+ * function main - print alphabet in lower letter
+ */
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
