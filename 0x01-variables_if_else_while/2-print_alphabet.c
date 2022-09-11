53b43686bb3034b669626c1b0be6a7ff90b7be07
shully5050 <shullamiteibeakalam2019@gmail.com>
2022-09-10 20:49:47 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+task 2 
+
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lower letter
+ * Return: Always 0 (Success)
+ * function main - print alphabet in lower letter
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
